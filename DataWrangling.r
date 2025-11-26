756656ea9236a9116a6128cbe0d6567aa1dace42
NoahYasbin <159480329+NoahYasbin@users.noreply.github.com>
2025-11-26 15:59:34 -0500

    Combining into team collumn
    Combining both team names and team into one column called team. For example, Team: Baltimore, TeamName: Ravens, would now be combined to be Team: Baltimore Ravens

--- DataWrangling.r
+++ DataWrangling.r
@@ -1,3 +1,14 @@
 attendance <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2020/2020-02-04/attendance.csv')
 standings <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2020/2020-02-04/standings.csv')
 games <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2020/2020-02-04/games.csv')
+
+attendance_clean <- attendance %>%
+  mutate(team = paste(team, team_name)) %>%  # combine into one column called 'team'
+  select(-team_name)                         # drop the old team_name column
+attendance_clean
+
+
+Standings_clean <- standings %>%
+  mutate(team = paste(team, team_name)) %>%  # combine into one column called 'team'
+  select(-team_name)                         # drop the old team_name column
+Standings_clean
