9a947faf7a230e5ce99ec8977fe13d2e7945f85b
Navinn88 <harvey.a.barnes@gmail.com>
2025-11-26 15:22:05 -0500

    Create DataWrangling.r

---
+++ DataWrangling.r
@@ -0,0 +1,3 @@
+attendance <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2020/2020-02-04/attendance.csv')
+standings <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2020/2020-02-04/standings.csv')
+games <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2020/2020-02-04/games.csv')
